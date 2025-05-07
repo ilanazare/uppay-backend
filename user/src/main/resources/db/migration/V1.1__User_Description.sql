4c1c7f245206e79e370e86a6bf21f3def188cf67
ilanazare <ivanazare94@gmail.com>
2025-05-07 08:14:08 -0300

    includes fee and migration modules

--- user/src/main/resources/db/migration/V1.1__User_Description.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    username                        VARCHAR(200)        NOT NULL   ,
-    password                        VARCHAR(200)        NOT NULL   ,
-    authorities                     VARCHAR(100)        NOT NULL   ,
-    PRIMARY KEY (username)
-);
