a8357077179633837195f669a9aad9971460e08d
ilanazare <ivanazare94@gmail.com>
2025-05-15 16:21:31 -0300

    includes table customer on migration module

---
+++ migration/src/main/resources/db/migration/V3.1__User_Description.sql
@@ -0,0 +1,5 @@
+CREATE TABLE customers (
+    customer                        VARCHAR(200)        NOT NULL   ,
+    email                           VARCHAR(200)        NOT NULL   ,
+    PRIMARY KEY (customer)
+);
